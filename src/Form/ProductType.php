71c6d87eb1fb336af6bf02ce24be60cade8d87d6
Alexandr Karelin <sanyasan.karelin.93@mail.ru>
2022-01-26 13:49:14 +0300

    add file type field to product form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -53,6 +54,14 @@ class ProductType extends AbstractType
                 'attr' => [
                     'class' => 'form-control',
                     'style' => 'overflow: hidden;',
+                ]
+            ])
+            ->add('newImage', FileType::class, [
+                'label' => 'Choose new image',
+                'required' => true,
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-control-file'
                 ]
             ])
             ->add('isPublished', CheckboxType::class, [
